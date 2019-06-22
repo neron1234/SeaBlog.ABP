c346268ef5aeade0a95fd9220a7763940ba62207
crybigsea <crybigsea@outlook.com>
2019-06-22 23:01:22 +0800

    Show blog detail

--- vue.index/src/main.ts
+++ vue.index/src/main.ts
@@ -4,6 +4,7 @@ import BootstrapVue from 'bootstrap-vue'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
 import store from './store/index'
+import './Resource/css/GitHubMarkDownStyle.css'
 
 Vue.config.productionTip = true;
 Vue.use(BootstrapVue)
